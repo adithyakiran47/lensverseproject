0d5a6dfd0ff5487a468c02f44577a5c859e1e7a6
Adithya Kiran V <adithya44kiran@gmail.com>
2025-11-05 22:29:22 +0530

    unwanted

--- lensverse-client/src/utils/api_20251104102925.js
+++
@@ -1,8 +0,0 @@
-import axios from 'axios';
-
-const api = axios.create({
-  baseURL: process.env.REACT_APP_API_BASE || '/api',
-  timeout: 15000
-});
-
-export default api;
--- lensverse-client/src/utils/api_20251104105006.js
+++
@@ -1,3 +0,0 @@
-import axios from 'axios';
-const base = process.env.REACT_APP_API_BASE || '/api';
-export default axios.create({ baseURL: base, timeout: 15000 });
